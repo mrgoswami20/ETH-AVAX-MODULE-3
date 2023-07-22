6831bfe989ce72c117dad72d528f7a6e12738480
Rohit Kumar Goswami <105840434+mrgoswami20@users.noreply.github.com>
2023-07-22 13:28:19 +0530

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const MyToken = await ethers.getContractFactory("MyToken");
+  const myToken = await MyToken.deploy("MyToken", "MTK");
+  await myToken.deployed();
+  console.log("MyToken deployed to:", myToken.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
